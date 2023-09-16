5ee17255085d2e3da761090b6fa6ff77e3554c95
Guru Das Srinagesh <gurooodas@gmail.com>
2023-09-16 13:20:18 -0700

    gbe: interfaces: Add Typeswitch!
    Note that the output is "type pointer to S"
    
    Signed-off-by: Guru Das Srinagesh <gurooodas@gmail.com>

--- interfaces/interface.go
+++ interfaces/interface.go
@@ -26,14 +26,29 @@ func Ishow(i I) {
 	i.Show()
 }
 
+func typecheck(i interface{}) {
+	switch i.(type) {
+	case F:
+		fmt.Println("Yay, type F!")
+	case S:
+		fmt.Println("Yay, type S!")
+	case *S:
+		fmt.Println("Yay, type pointer to S!")
+	default:
+		fmt.Println("Unrecognized type")
+	}
+}
+
 func main() {
 	var i I
 
 	i = F{0.35}
 	i.Show()
 	Ishow(i) // The interface is the argument
+	typecheck(i)
 
 	i = &S{"test"}
 	i.Show()
 	Ishow(i) // The interface is the argument
+	typecheck(i)
 }
